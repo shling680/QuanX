3516c62db361996774f33f22a3f9f318148b9343
shling680 <84358462+shling680@users.noreply.github.com>
2023-08-28 15:37:01 +0800

    Update yanluo.js

--- scripts/yanluo.js
+++ scripts/yanluo.js
@@ -1,11 +1,11 @@
 /***** ***** ***** ***** *****
 
 [task_local]
-*/60 * * * * https://raw.githubusercontent.com/shling680/QuanX/main/scripts/yanluo.js
+1 0 * * * * https://raw.githubusercontent.com/shling680/QuanX/main/scripts/yanluo.js
 
 ***** ***** ***** ***** *****/
 
 
-const yanluo = new Date()+"\n时不待我,争分夺秒；";
-$notify("阎王签到", "👻 签到成功 寿元已减少1小时", yanluo);
+const yanluo = new Date()+"\n时不待我,争分夺秒";
+$notify("阎王签到", "👻 签到成功 寿元减少1天", yanluo);
 $done();
