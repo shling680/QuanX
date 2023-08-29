58e2c942155c431ea3e987820730d3001740f18d
shling680 <84358462+shling680@users.noreply.github.com>
2023-08-29 13:35:33 +0800

    Create jrcx.js

---
+++ scripts/jrcx.js
@@ -0,0 +1,10 @@
+/***** ***** ***** ***** *****
+
+[task_local]
+0 6 * * * https://raw.githubusercontent.com/shling680/QuanX/main/scripts/jrcx.js, tag=黄历, enabled=true
+
+***** ***** ***** ***** *****/
+
+const jrurl = {"open-url": "http://mobile.51wnl-cq.com/huangli_tab_h5/"}
+$notify("黄历", "吉日查询", "点击查看今日详情...", jrurl);
+$done();
